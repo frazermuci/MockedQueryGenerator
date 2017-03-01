7eab1adf122a0ca81bb976c8bd8dd1ea0f4361dd
aserran <agserran@uci.edu>
2017-03-01 12:08:47 -0800

    Add files via upload

---
+++ ActivAID DB Link/ActivAID DB Link/activaidConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivAID_DB_Link
+{
+    class activaidConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            string dblocation = "Server=.\\SQLEXPRESS;Database=ActivAID DB;Integrated Security=true";
+
+            SqlConnection conn = new SqlConnection(dblocation);
+            return conn;
+        }
+    }
+}
