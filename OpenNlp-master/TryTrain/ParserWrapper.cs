f324528842e5af823c14cbfc983a823994d90423
MATTHEWFRAZER <mfrazeriguess@gmail.com>
2017-03-05 10:05:21 -0800

    extract key words from files to generate SIML model

--- OpenNlp-master/TryTrain/ParserWrapper.cs
+++ OpenNlp-master/TryTrain/ParserWrapper.cs
@@ -24,11 +24,11 @@ namespace Parser
 		{
 			foreach(string file in filePaths)
 			{
-				parsedCHMs[file] = new ParsedCHM(filePath);
+				parsedCHMs[file] = new ParsedCHM(file);
 			}
 		}
 		
-		private void iterateThroughBlocks(string filePath, List<Element> blocks)
+		private void insertBlocksIntoDB(string filePath, List<List<Element>> blocks)
 		{
 			foreach (List<Element> block in blocks)
             {
@@ -50,7 +50,7 @@ namespace Parser
             }
 		}
 		
-		private void iterateThroughHREFS(string filePath, List<string> hrefs)
+		private void insertHREFSOIntoDB(string filePath, List<string> hrefs)
 		{
 			foreach (string href in pair.Second.hrefs)
 			{
@@ -66,10 +66,67 @@ namespace Parser
 			
 			foreach (Pair<string, ParsedCHM> pair in parsedCHMs)
 			{
-				iterateThroughBlocks(pair.First, pair.Second.blocks);
-				iterateThroughHREFS(pair.Second.title, pair.Second.hrefs);
-				//SIML
+				insertBlocksIntoDB(pair.First, pair.Second.blocks);
+				insertHREFSOIntoDB(pair.Second.title, pair.Second.hrefs);
 			}
         }
+		
+		private insertIntoOccuranceDict(ref Dictionary<string, int> occuranceOfWord, Element element)
+		{
+			if (element.name != "img")
+            {
+				foreach(string word in element.data.split(" "))//really need to fix this
+				{
+					if(occuranceOfWord.ContainsKey())
+					{
+						occuranceOfWord[word]= occuranceOfWord[word]+1;
+					}
+					else
+					{
+						occuranceOfWord[word] =1;
+					}
+				}
+            }
+		}
+		
+		private List<string> getKeyWords(List<List<Element>> elements)
+		{
+			Dictionary<string, int> occuranceOfWord = new Dictionary<string, int>();
+			List<string> keywords = new List<string>();
+			
+			foreach (List<Element> block in blocks)
+            {
+                foreach (Element element in block)
+                {
+                    insertIntoOccuranceDict(ref occuranceOfWord, element);
+                }
+            }
+			
+			for(var i =0; i < 5; ++i)
+			{
+				string maxWord = occuranceOfWord.Max(x => x.Second).First;
+				keywords.Add(maxWord);
+				occuranceOfWord.Remove(maxWord);
+			}
+			return keywords;
+		}
+		
+		public void genModel()
+		{
+			List<string> keywords = new List<string>();
+			List<string> responseFileNames = new List<string>();
+			string response;
+ 			foreach (Pair<string, ParsedCHM> pair in parsedCHMs)
+			{
+				responseFileNames.Add(pair.First);
+				keywords.AddRange(getKeyWords(pair.Second.blocks));
+			}
+			response = responseFileNames.Join(";", responseFileNames.ToArray());
+			Console.WriteLine(response);
+			foreach(string keyword in keywords)
+			{
+				Console.WriteLine(keyword);
+			}
+		}
     }
 }
